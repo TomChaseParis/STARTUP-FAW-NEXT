a42baf95206b49d89891d5cc871b86453f99ec6c
TomChaseParis <chasethomasparis@gmail.com>
2025-09-06 10:39:13 +0200

    Add ActivityCard component and link BeginnerPage activities with titles & descriptions

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,4 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+
 import Breadcrumb from "@/components/Common/Breadcrumb";
 
 import { Metadata } from "next";
@@ -17,8 +16,7 @@ const AboutPage = () => {
         pageName="About Page"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
       />
-      <AboutSectionOne />
-      <AboutSectionTwo />
+     
     </>
   );
 };
