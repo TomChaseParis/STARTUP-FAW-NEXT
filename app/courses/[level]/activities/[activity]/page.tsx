54448788313130a8f80fbf3ee352df0a3cb9fa1d
TomChaseParis <chasethomasparis@gmail.com>
2026-02-18 10:47:49 +0100

    :(feat): refonte globale du code

--- app/courses/[level]/activities/[activity]/page.tsx
+++ app/courses/[level]/activities/[activity]/page.tsx
@@ -1,16 +1,24 @@
 import dynamic from "next/dynamic";
 import { notFound } from "next/navigation";
+import path from "path";
 
-export default async function Page({ params }) {
+export default function Page({ params }: { params: { level: string; activity: string } }) {
   const Activity = dynamic(
     () =>
       import(
-        `@/components/courses/activities/${params.level}/${params.activity}/Activity`
+        `../../../../../components/courses/activities/${params.level}/${params.activity}/Activity`
       ),
     { ssr: false }
   );
+  
 
-  return  <div className="bg-white pt-[130px]">
-  <Activity />
-</div>
+  if (!Activity) {
+    notFound();
+  }
+
+  return (
+    <div className="bg-white pt-[130px]">
+      <Activity />
+    </div>
+  );
 }
