024f4e16c4678026baeccdb92de65f8a191e0984
TomChaseParis <chasethomasparis@gmail.com>
2026-02-18 11:55:01 +0100

    Add components folder

--- app/api/courses/activities/route.ts
+++ app/api/courses/activities/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import path from "path";
 import { promises as fs } from "fs";
 
+export const dynamic = "force-dynamic"; // ✅ IMPORTANT
+
 export async function GET(
   req: Request,
   { params }: { params: { level: string } }
